0c320c4e67122d353ebdc75d6f25e1bd1a860a11
knareshkumar-dev <knareshkumar1910@gmail.com>
2026-01-12 13:20:53 +0530

    Added the React and ReactDOM CDN, create a new element and render it

--- 01-inception/app.js
+++ 01-inception/app.js
@@ -1,8 +1,16 @@
-// CREATE A NEW ELEMENT - h1
+
+
+/* // CREATE A NEW ELEMENT - h1
 let heading1 = document.createElement("h1");
 heading1.innerHTML = "Hello World...!";
 
 // APPEND THE NEW ELEMENT CREATED
 let root = document.getElementById("root");
 root.appendChild(heading1);
+ */
 
+const heading1 = React.createElement("h1", {}, "Hello World");
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+
+root.render(heading1);
