d652f2cbd6e6a60d34db9abe52e5f1fdd3743e44
knareshkumar-dev <knareshkumar1910@gmail.com>
2026-01-12 12:13:45 +0530

    Created a new heading element using dom and append to it

---
+++ 01-inception/app.js
@@ -0,0 +1,8 @@
+// CREATE A NEW ELEMENT - h1
+let heading1 = document.createElement("h1");
+heading1.innerHTML = "Hello World...!";
+
+// APPEND THE NEW ELEMENT CREATED
+let root = document.getElementById("root");
+root.appendChild(heading1);
+
