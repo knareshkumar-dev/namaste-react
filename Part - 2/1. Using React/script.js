3aa3ceba2d77dfe01032919437c7524cddaf5f8d
knareshkumar-dev <knareshkumar1910@gmail.com>
2026-01-30 00:19:24 +0530

    Completed 1st & 2nd Part

---
+++ Part - 2/1. Using React/script.js
@@ -0,0 +1,6 @@
+// Simple Hello World 
+const heading1 = React.createElement("h1", {}, "Hello World from React JS");
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(heading1);
+
+
